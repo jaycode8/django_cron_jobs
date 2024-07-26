67b21f87f18d4847f93258f63af1c682fdc0c0fd
jaycode8 <jamesmumo443@gmail.com>
2024-07-26 12:07:50 +0300

    updates

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -8,17 +8,31 @@ import os
 load_dotenv()
 sender = os.getenv("sender")
 password = os.getenv("password")
+recepient = os.getenv("recepient")
 
 
 def send_daily_mail():
     service, message = set_message()
     message["Subject"] = "Daily cron message"
     message["From"] = sender
-    message["To"] = "jamesmumo443@gmail.com"
-    message.set_content("Hey this is your daily email message")
+    message["To"] = recepient
+
+    # Read the HTML file
+    # try:
+    #     with open("./test.html", "r") as f:
+    #         html_content = f.read()
+    # except Exception as e:
+    #     print(f"Error reading HTML file: {e}")
+    #     return False
+
+    message.set_content("Hey there")
+    # message.add_alternative(html_content, subtype='html')
+
     try:
         service.send_message(message)
+        print("email sent")
     except Exception as e:
+        print(e)
         return False
     service.quit()
     return True
